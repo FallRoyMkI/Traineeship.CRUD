115978b62a9052d42b3783ac301a7ce8b769a1f5
Fall <pilimecka@mail.ru>
2023-04-30 21:56:02 +0300

    Fixed URL

--- WorkTest/Program.cs
+++ WorkTest/Program.cs
@@ -4,4 +4,4 @@ CreateHostBuilder(args).Build().Run();
 
 static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
     .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>()
-        .UseKestrel().UseUrls("http://localhost:5001"));
+        .UseKestrel().UseUrls("http://localhost:5001/swagger"));
