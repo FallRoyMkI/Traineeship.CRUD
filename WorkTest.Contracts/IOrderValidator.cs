a699495c6fbcfa9fed0cbd16e5b8edf810214b82
Fall <pilimecka@mail.ru>
2023-05-01 14:36:35 +0300

    move interfaces to project for contracts

---
+++ WorkTest.Contracts/IOrderValidator.cs
@@ -0,0 +1,9 @@
+﻿using WorkTest.Models.Model;
+
+namespace WorkTest.Contracts;
+
+public interface IOrderValidator
+{
+    public void OrderCreateModelValidator(OrderModel order);
+    public void OrderUpdateModelValidator(OrderModel order);
+}
